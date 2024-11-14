b917fa60fe12ab78417571b35c4436a1f4de7437
TugAh <tunganh97tuaf@gmail.com>
2024-11-14 08:14:15 +0700

    Bai tap ve hinh

---
+++ Java/vongLap/bai19MuaBia/result.java
@@ -0,0 +1,37 @@
+package vongLap.bai19MuaBia;
+
+import java.util.Scanner;
+
+public class result {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(tongSoChaiBia(n));
+    }
+
+    public static int soChaiBiaCoTheMua(int n){
+        int beerPrice = 28;
+        int soChaiBia = n / beerPrice;
+        return soChaiBia;
+    }
+
+    public static int soChaiBiaCoTheDoi(int soChaiBia){
+        int doiBia = 0;
+
+        while (soChaiBia >= 3){
+            int doiVoLayBia = soChaiBia /3;
+
+            doiBia = doiBia +  doiVoLayBia;
+
+            soChaiBia = soChaiBia - (doiVoLayBia * 3) + doiVoLayBia;
+        }
+
+        return doiBia;
+    }
+
+    public static int tongSoChaiBia(int n){
+        return soChaiBiaCoTheDoi(soChaiBiaCoTheMua(n)) + soChaiBiaCoTheMua(n);
+    }
+}

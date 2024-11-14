c48c9bc706ee3856d00fed6ba4ee444c1779ea47
TugAh <tunganh97tuaf@gmail.com>
2024-11-14 22:25:01 +0700

    Complete_ve hinh tam giac can

---
+++ Java/vongLap/bai32VeHinh4/result.java
@@ -0,0 +1,29 @@
+package vongLap.bai32VeHinh4;
+
+import java.util.Scanner;
+
+public class result {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        imageNumber1(n);
+    }
+
+    public static void imageNumber1 (int n){
+
+
+        for (int i = 0; i < n; i++){
+
+            for (int j = 0; j <= i; j++){
+
+                char letter = (char) ('A' + i);
+
+                System.out.print(letter);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}

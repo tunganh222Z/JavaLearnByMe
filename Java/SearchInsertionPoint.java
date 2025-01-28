2d7c93f76e13089f95a669a59a30eb035420544d
TugAh <tunganh97tuaf@gmail.com>
2025-01-28 14:50:04 +0700

    search insertion point

--- Java/SearchInsertionPoint.java
+++ Java/SearchInsertionPoint.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class SearchInsertionPoint {
+    public static void main(String[] args) {
+
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+            int index = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == target){
+                    index = i;
+                } else {
+                    if (nums[i] < target){
+                        index = i+1;
+                    }
+                }
+            }
+            return index;
+    }
 }

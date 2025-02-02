388bd14f3907850299f330a11e67fdf93911f82a
TugAh <tunganh97tuaf@gmail.com>
2025-02-02 17:52:27 +0700

    Number recent call 933 _ Linked list and Queue java library

--- Java/StackQueue/StackQueueJava/_933_NumberOfRecentCalls.java
+++ Java/StackQueue/StackQueueJava/_933_NumberOfRecentCalls.java
@@ -32,40 +32,38 @@ public class _933_NumberOfRecentCalls {
     }
 
 
-    static class RecentCounter1 { // using linked list
+    static class RecentCounter1 {
 
         private class node {
             int val;
             private node next;
 
-            node(int value) {
+            node(int value){
                 this.val = value;
             }
         }
-
         node head;
         node tail;
-
         public RecentCounter1() {
             head = tail = null;
         }
 
         public int ping(int t) {
             node newNode = new node(t);
-            if (head == null) {
+            if (head == null){
                 head = tail = newNode;
             } else {
                 tail.next = newNode;
                 tail = tail.next;
             }
 
-            while (head != null && head.val < t - 3000) {
+            while (head != null && head.val < t - 3000){
                 head = head.next;
             }
 
             node temp = head;
             int count = 0;
-            while (temp != null) {
+            while(temp != null){
                 count++;
                 temp = temp.next;
             }

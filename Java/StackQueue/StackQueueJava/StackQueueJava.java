08a6222ffd2d1cdc9fffefd2e5fc233c67863f2b
TugAh <tunganh97tuaf@gmail.com>
2025-02-02 17:56:17 +0700

    Stack Queue java library

--- Java/StackQueue/StackQueueJava/StackQueueJava.java
+++ Java/StackQueue/StackQueueJava/StackQueueJava.java
@@ -1,4 +1,31 @@
 package StackQueue.StackQueueJava;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class StackQueueJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Stack<Integer> myStack = new Stack<>();
+
+        for (int i =0; i < n; i++){
+            myStack.push(sc.nextInt());
+        }
+
+        for (Integer integer : myStack){
+            System.out.println(integer);
+        }
+        System.out.println("============== pop 1 element ==============");
+        System.out.println("pop ra phan tu "+ myStack.pop());
+        for (Integer integer : myStack){
+            System.out.println(integer);
+        }
+        System.out.println("peek hien tai : "+ myStack.peek());
+
+        Queue<Integer> queue = new LinkedList<>();
+    }
 }

bffebf211e55c96dfedc7a02e15c7a3e471a054d
TugAh <tunganh97tuaf@gmail.com>
2025-02-10 22:16:48 +0700

    Binary tree knowledge

---
+++ Java/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package tree;
+
+public class TreeNode {
+    private int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val){
+        this.val = val;
+    }
+}
+
---
+++ Java/tree/test.java
@@ -0,0 +1,10 @@
+package tree;
+
+public class test {
+    public static void main(String[] args) {
+     MyBinaryTree myBinaryTree = new MyBinaryTree();
+
+     myBinaryTree.init();
+     System.out.println("Done");
+    }
+}

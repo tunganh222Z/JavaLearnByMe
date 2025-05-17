176cfeec2d9df092af740bca9ebc2531fa1ada5e
TugAh <tunganh97tuaf@gmail.com>
2025-05-17 17:52:06 +0700

    "Build utilities method"

---
+++ Java/Utilities/Formater.java
@@ -0,0 +1,8 @@
+package Utilities;
+
+public class Formater {
+    public static final String YYYY_MM_DD_WITHOUT_MINUS = "yyyyMMdd";
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+    public static final String YYYY_MM_DD_WITH_SLASH= "dd/MM/yyyy";
+    public static final String DD_MMM_YYYY = "dd MMM yyyy";
+}
---
+++ Java/Utilities/JsonSupport.java
@@ -0,0 +1,8 @@
+package Utilities;
+
+public class JsonSupport {
+    String instance;
+    public JsonSupport(String instance) {
+        this.instance = instance;
+    }
+}
